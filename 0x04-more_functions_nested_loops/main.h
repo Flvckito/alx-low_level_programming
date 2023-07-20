0702c85918242ffbceec752f141491350ac35456
Flvckito <bradleyterg@gmail.com>
2023-07-20 04:53:24 -0700

    mul

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -24,4 +24,13 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-#endif 
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
+int mul(int a, int b);
+
+#endif
