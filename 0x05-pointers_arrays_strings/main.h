82aea33884327e6e4df419741731861ba15ae515
Flvckito <bradleyterg@gmail.com>
2023-07-24 08:02:30 -0700

    puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -52,4 +52,11 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str);
+
 #endif
