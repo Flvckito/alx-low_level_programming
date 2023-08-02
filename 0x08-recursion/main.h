a6834115092143e233f403b057f21318dd2df64c
Flvckito <bradleyterg@gmail.com>
2023-08-02 02:13:17 -0700

    stlren

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -22,4 +22,12 @@ void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
 
+/**
+ * _strlen_recursion - return string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s);
+
 #endif
